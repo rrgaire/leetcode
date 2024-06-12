347537a26a2abf590b8c668ebe8de8e9467f36cb
rrgaire <rebati.gaire@gmail.com>
2024-06-12 11:57:05 -0500

    23. Merge k Sorted Lists

---
+++ Topic 6: Linked List/23. Merge k Sorted Lists.py
@@ -0,0 +1,87 @@
+"""
+23. Merge k Sorted Lists
+Solved
+Hard
+Topics
+Companies
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        # time: O(nlogk) | space: O(n)
+        if not lists or len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            mergedList = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                merged = self.merge2Lists(l1, l2)
+                mergedList.append(merged)
+            lists = mergedList
+        return lists[0]
+
+    def merge2Lists(self, list1, list2):
+        dummy = ListNode()
+        cur = dummy
+
+        while list1 and list2:
+
+            if list1.val <= list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+        if list1:
+            cur.next = list1
+        if list2:
+            cur.next = list2
+            
+        return dummy.next
+        
