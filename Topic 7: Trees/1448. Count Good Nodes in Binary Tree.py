01a47f64d4e26478fccb9a0e15432a9f1bb1cb20
rrgaire <rebati.gaire@gmail.com>
2024-06-18 08:31:53 -0500

    1448. Count Good Nodes in Binary Tree

---
+++ Topic 7: Trees/1448. Count Good Nodes in Binary Tree.py
@@ -0,0 +1,85 @@
+"""
+1448. Count Good Nodes in Binary Tree
+Solved
+Medium
+Topics
+Companies
+Hint
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+Example 2:
+
+
+
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+Example 3:
+
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+ 
+
+Constraints:
+
+The number of nodes in the binary tree is in the range [1, 10^5].
+Each node's value is between [-10^4, 10^4].
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        
+        # # time: O(n) | space: O(1)
+        # def dfs(node, maxVal):
+
+        #     if not node:
+        #         return 0
+
+        #     res = 1 if node.val >= maxVal else 0
+        #     maxVal = max(maxVal, node.val)
+                
+            
+        #     res += dfs(node.left, maxVal)
+        #     res += dfs(node.right, maxVal)
+        #     return res
+
+        # return dfs(root, root.val)
+
+        res = 0
+        def dfs(node, large):
+            nonlocal res
+            if not node:
+                return None
+            
+            if node.val >= large:
+                res += 1
+            large = max(large, node.val)
+
+            dfs(node.left, large)
+            dfs(node.right, large)
+
+        dfs(root, root.val)
+        return res
