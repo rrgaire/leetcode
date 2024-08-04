b76a67ff9b2b626bb88c816c95374442e84c2e65
rrgaire <rebati.gaire@gmail.com>
2024-08-04 15:19:14 -0500

    48. Rotate Image

---
+++ Topic: 17: Math & Geometry/48. Rotate Image.py
@@ -0,0 +1,70 @@
+"""
+48. Rotate Image
+Solved
+Medium
+Topics
+Companies
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+Example 2:
+
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ 
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
+
+"""
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        left, right = 0, len(matrix) - 1
+
+        # while left < right:
+        #     top, bottom = left, right
+        #     for i in range(right - left):
+
+        #         tmp, matrix[top + i][right] = matrix[top + i][right], matrix[top][left + i]
+
+        #         tmp, matrix[bottom][right - i] = matrix[bottom][right - i], tmp
+
+        #         tmp, matrix[bottom - i][left] = matrix[bottom - i][left], tmp
+
+        #         tmp, matrix[top][left + i] = matrix[top][left + i], tmp
+
+        #     left += 1
+        #     right -= 1
+        
+        # left, right = 0, len(matrix) - 1
+
+        while left < right:
+            top, bottom = left, right
+            for i in range(right - left):
+
+                tmp = matrix[top][left + i]
+                matrix[top][left + i] = matrix[bottom - i][left]
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+                matrix[bottom][right - i] = matrix[top + i][right]
+                matrix[top + i][right] = tmp
+
+            left += 1
+            right -= 1
+
