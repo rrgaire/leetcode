c9c3a20d002967a37e85d58a7ef09547bddc5075
rrgaire <rebati.gaire@gmail.com>
2024-08-15 13:48:40 -0500

    1984. Minimum Difference Between Highest and Lowest of K Scores

---
+++ Topic 02: Two Pointers/1984. Minimum Difference Between Highest and Lowest of K Scores.py
@@ -0,0 +1,57 @@
+"""
+1984. Minimum Difference Between Highest and Lowest of K Scores
+Solved
+Easy
+Topics
+Companies
+Hint
+You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given an integer k.
+
+Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
+
+Return the minimum possible difference.
+
+ 
+
+Example 1:
+
+Input: nums = [90], k = 1
+Output: 0
+Explanation: There is one way to pick score(s) of one student:
+- [90]. The difference between the highest and lowest score is 90 - 90 = 0.
+The minimum possible difference is 0.
+Example 2:
+
+Input: nums = [9,4,1,7], k = 2
+Output: 2
+Explanation: There are six ways to pick score(s) of two students:
+- [9,4,1,7]. The difference between the highest and lowest score is 9 - 4 = 5.
+- [9,4,1,7]. The difference between the highest and lowest score is 9 - 1 = 8.
+- [9,4,1,7]. The difference between the highest and lowest score is 9 - 7 = 2.
+- [9,4,1,7]. The difference between the highest and lowest score is 4 - 1 = 3.
+- [9,4,1,7]. The difference between the highest and lowest score is 7 - 4 = 3.
+- [9,4,1,7]. The difference between the highest and lowest score is 7 - 1 = 6.
+The minimum possible difference is 2.
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 1000
+0 <= nums[i] <= 105
+
+"""
+
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+        
+        l = 0
+        r = k - 1
+        res = nums[-1]
+
+        while r < len(nums):
+            res = min(res, nums[r] - nums[l])
+            l += 1
+            r += 1
+        return res
