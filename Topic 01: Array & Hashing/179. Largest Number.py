edfda68d00afecda730494ca224563cb55c6f19a
rrgaire <rebati.gaire@gmail.com>
2024-08-16 15:17:47 -0500

    179. Largest Number

---
+++ Topic 01: Array & Hashing/179. Largest Number.py
@@ -0,0 +1,44 @@
+"""
+179. Largest Number
+Solved
+Medium
+Topics
+Companies
+Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+Since the result may be very large, so you need to return a string instead of an integer.
+
+ 
+
+Example 1:
+
+Input: nums = [10,2]
+Output: "210"
+Example 2:
+
+Input: nums = [3,30,34,5,9]
+Output: "9534330"
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 109
+
+"""
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+
+        def helper(s, t):
+            if int(s + t) > int(t + s):
+                return -1
+            return 1
+
+        
+        nums = sorted(map(str, nums), key = cmp_to_key(helper))
+
+        return "".join(nums) if nums[0] != '0' else '0'
+
+
+        
