531d12a9bfaf7c268ee31eea608df9996d20870f
rrgaire <rebati.gaire@gmail.com>
2024-08-21 15:04:47 -0500

    18. 4Sum

---
+++ Topic 02: Two Pointers/18. 4Sum.py
@@ -0,0 +1,92 @@
+"""
+18. 4Sum
+Solved
+Medium
+Topics
+Companies
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Example 2:
+
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+
+"""
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+
+        res = []
+        nums.sort()
+
+        for i in range(len(nums) - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums) - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                
+                k = j + 1
+                l = len(nums) - 1
+
+                while k < l:
+                    total = nums[i] + nums[j] + nums[k] + nums[l]
+                    if total > target:
+                        l -= 1
+                    elif total < target:
+                        k += 1
+                    else:
+                        res.append([nums[i], nums[j], nums[k], nums[l]])
+
+                        k += 1
+                        while k < l and nums[k] == nums[k - 1]:
+                            k += 1
+                        
+        
+        return res
+            
+
+class Solution:
+    def fourSum(self, nums, target):
+        def findNsum(l, r, target, N, result, results):
+            if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:  
+                return
+            if N == 2: 
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else:
+                for i in range(l, r+1):
+                    if i == l or (i > l and nums[i-1] != nums[i]):
+                        findNsum(i+1, r, target-nums[i], N-1, result+[nums[i]], results)
+
+        nums.sort()
+        results = []
+        findNsum(0, len(nums)-1, target, 4, [], results)
+        return results
