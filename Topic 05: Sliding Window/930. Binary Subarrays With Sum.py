c72714cc7d728b3c65fff423e788dcbaa8716f42
rrgaire <rebati.gaire@gmail.com>
2024-09-02 19:46:11 -0500

    930. Binary Subarrays With Sum

---
+++ Topic 05: Sliding Window/930. Binary Subarrays With Sum.py
@@ -0,0 +1,54 @@
+"""
+930. Binary Subarrays With Sum
+Solved
+Medium
+Topics
+Companies
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+A subarray is a contiguous part of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,1,0,1], goal = 2
+Output: 4
+Explanation: The 4 subarrays are bolded and underlined below:
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+Example 2:
+
+Input: nums = [0,0,0,0,0], goal = 0
+Output: 15
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+nums[i] is either 0 or 1.
+0 <= goal <= nums.length
+
+"""
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+
+        def helper(g):
+
+            l = 0
+            curr = 0
+            res = 0
+            for r in range(len(nums)):
+                curr += nums[r]
+                
+                while l <= r and curr > g:
+                    curr -= nums[l]
+                    l += 1
+                
+                res += (r - l + 1)
+            return res
+        
+        return helper(goal) - helper(goal - 1)
