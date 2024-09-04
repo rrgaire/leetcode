d9fec88fbf132ac74bb55e7de818b2f6a8fd1de2
rrgaire <rebati.gaire@gmail.com>
2024-09-04 12:02:15 -0500

    2962. Count Subarrays Where Max Element Appears at Least K Times "

---
+++ Topic 05: Sliding Window/2962. Count Subarrays Where Max Element Appears at Least K Times.py
@@ -0,0 +1,54 @@
+"""
+2962. Count Subarrays Where Max Element Appears at Least K Times
+Solved
+Medium
+Topics
+Companies
+You are given an integer array nums and a positive integer k.
+
+Return the number of subarrays where the maximum element of nums appears at least k times in that subarray.
+
+A subarray is a contiguous sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,2,3,3], k = 2
+Output: 6
+Explanation: The subarrays that contain the element 3 at least 2 times are: [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+Example 2:
+
+Input: nums = [1,4,2,1], k = 3
+Output: 0
+Explanation: No subarray contains the element 4 at least 3 times.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+1 <= k <= 105
+
+"""
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+
+        res = 0
+        l = r = c = 0
+        target = max(nums)
+        
+        for r in range(len(nums)):
+
+            if nums[r] == target:
+                c += 1
+            
+            while c == k:
+                if nums[l] == target:
+                    c -= 1
+                l += 1
+            res += l
+
+        return res
+        
