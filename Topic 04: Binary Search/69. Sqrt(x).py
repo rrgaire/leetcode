e8e0b50688ab9ac6818f55b2df9b15c5bdca3800
rrgaire <rebati.gaire@gmail.com>
2024-09-15 14:32:14 -0500

    69. Sqrt(x)

---
+++ Topic 04: Binary Search/69. Sqrt(x).py
@@ -0,0 +1,49 @@
+"""
+69. Sqrt(x)
+Solved
+Easy
+Topics
+Companies
+Hint
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+
+"""
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        l = 0
+        r = x
+
+        while l <= r:
+            m = l + (r - l) // 2
+            sq = m ** 2
+            if sq > x:
+                r = m - 1
+            elif sq < x:
+                l = m + 1
+            else:
+                return m
+        return r
+        
