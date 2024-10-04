f92fa74c863347cd759c50c977102fe239bb913d
rrgaire <rebati.gaire@gmail.com>
2024-10-04 11:02:22 -0500

    144. Binary Tree Preorder Traversal

---
+++ Topic 07: Trees/144. Binary Tree Preorder Traversal.py
@@ -0,0 +1,94 @@
+"""
+144. Binary Tree Preorder Traversal
+Solved
+Easy
+Topics
+Companies
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,2,3]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [1,2,4,5,6,7,3,8,9]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Recursive
+        res = []
+
+        def dfs(node):
+            if not node:
+                return
+            
+            res.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return res
+
+        # Iterative
+
+        res = []
+        stack = []
+        cur = root
+
+        while cur or stack:
+            if cur:
+                res.append(cur.val)
+                stack.append(cur.right)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+
+        return res
+
+        
