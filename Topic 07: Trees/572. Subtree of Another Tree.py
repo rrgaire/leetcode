2a9d80f620425ad3c7e63dccbef422c846b0a601
rrgaire <rebati.gaire@gmail.com>
2024-10-08 15:56:48 -0500

    572. Subtree of Another Tree "

--- Topic 07: Trees/572. Subtree of Another Tree.py
+++ Topic 07: Trees/572. Subtree of Another Tree.py
@@ -59,3 +59,31 @@ class Solution:
 
         return (self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right))
         
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def isSameTree(self, node1, node2):
+        if not node1 and not node2:
+            return True
+        
+        if (not node1 and node2) or (node1 and not node2) or (node1.val != node2.val):
+            return False
+        
+        return self.isSameTree(node1.left, node2.left) and self.isSameTree(node1.right, node2.right)
+
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+
+        if self.isSameTree(root, subRoot):
+            return True
+
+        
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
