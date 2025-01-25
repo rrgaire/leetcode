80e1ed9c6c3c84e8dfd0b889558ec0f1ac4828d7
rrg-assembli <rebati.gaire@gmail.com>
2025-01-25 15:11:37 -0600

    332. Reconstruct Itinerary

---
+++ Topic: 18: Advanced Graph/332. Reconstruct Itinerary.py
@@ -0,0 +1,84 @@
+"""
+332. Reconstruct Itinerary
+Solved
+Hard
+Topics
+Companies
+You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
+
+All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+
+For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+
+ 
+
+Example 1:
+
+
+Input: tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+Output: ["JFK","MUC","LHR","SFO","SJC"]
+Example 2:
+
+
+Input: tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"] but it is larger in lexical order.
+ 
+
+Constraints:
+
+1 <= tickets.length <= 300
+tickets[i].length == 2
+fromi.length == 3
+toi.length == 3
+fromi and toi consist of uppercase English letters.
+fromi != toi
+
+
+"""
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = {src: [] for src, dst in tickets}
+        tickets.sort()
+        for src, dst in tickets:
+            adj[src].append(dst)
+
+        res = ["JFK"]
+        def dfs(src):
+            if len(res) == len(tickets) + 1:
+                return True
+            if src not in adj:
+                return False
+
+            temp = list(adj[src])
+            for i, v in enumerate(temp):
+                adj[src].pop(i)
+                res.append(v)
+                if dfs(v): return True
+                adj[src].insert(i, v)
+                res.pop()
+            return False
+            
+        dfs("JFK")
+        return res
+
+        # Approach 2
+        adj = {}
+        for src, dst in sorted(tickets, reverse=True):
+            if src not in adj:
+                adj[src] = []
+            adj[src].append(dst)
+        
+
+        stack = ['JFK']
+        res = []
+
+        while stack:
+            if stack[-1] in adj and adj[stack[-1]]:
+                stack.append(adj[stack[-1]].pop())
+            else:
+                res.append(stack.pop())
+        
+        return res[::-1]
